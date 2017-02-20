d43fe18c140cf9ee5601f9f2bd0ab45236a7da61
esheetal <esheetal@yahoo.com>
2017-02-20 21:57:39 +0530

    First commit

---
+++ dockerized_claims_adjudication/src/main/java/com/tutorial/ClaimsAdjudicationApplication.java
@@ -0,0 +1,12 @@
+package com.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaimsAdjudicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaimsAdjudicationApplication.class, args);
+	}
+}
